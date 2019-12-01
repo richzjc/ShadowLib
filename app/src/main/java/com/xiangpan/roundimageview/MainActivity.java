5543e85685c62dc1ca29f610fcee19f0701f109b
richzjc <zhangjianchuan@wallstreetcn.com>
2019-12-01 20:12:17 +0800

    add files

--- app/src/main/java/com/xiangpan/roundimageview/MainActivity.java
+++ app/src/main/java/com/xiangpan/roundimageview/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
     }
 }
